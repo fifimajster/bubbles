188b32e8478a31eeb43837949eb48a488c0ca2a9
fifimajster <filipsondej@protonmail.com>
2019-03-31 00:42:31 +0100

    add option to change profile better exception handling add docstring

--- bubbles.py
+++ bubbles.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import argparse
 import glob
 import json
 import os
@@ -33,16 +34,16 @@ def _get_db_cursor(db_name, profile, use_copy=True):
     """Return cursor to database.
     
     Arguments:
-        db_name {[type]} -- {places|cookies} name of the database
-        profile {[type]} -- name of your firefox profile
+        db_name {str} -- {places|cookies} name of the database
+        profile {str} -- name of your firefox profile
     
     Keyword Arguments:
         use_copy {bool} -- should use a copy instead of the original database (default: {True})
     
     Returns:
         sqlite3.Cursor -- Cursor for the copied database.
-
     """
+
     filename = _get_filename_safely(f'.mozilla/firefox/{profile}/{db_name}.sqlite')
 
     if use_copy:
@@ -55,6 +56,12 @@ def _get_db_cursor(db_name, profile, use_copy=True):
 
 
 def get_container_identities(profile):
+    """Get data about firefox containers.
+    
+    Arguments:
+        profile {str} -- firefox profile from which to get profiles
+    """
+
     filename = _get_filename_safely(f'.mozilla/firefox/{profile}/containers.json')
     with open(filename) as file:
         container_data = json.load(file)
@@ -89,8 +96,7 @@ def _correct_id(raw_id):
     return re.match(r'[0-9A-Za-z-_]{11}', raw_id)
 
 
-def main():
-    profile = 'synced'
+def main(profile):
     print('')
     try:
         from pyfiglet import figlet_format
@@ -103,7 +109,8 @@ def main():
     print(f'{"id":<13} name')
     print('-' * 30)
     raw_id = get_youtube_id(cursor, '')
-    print(f'{raw_id:<13} default')
+    if raw_id is not None:
+        print(f'{raw_id:<13} default')
     for identity in get_container_identities(profile):
         userContextId = identity['userContextId']
         name = identity['name']
@@ -112,6 +119,7 @@ def main():
             print(f'{raw_id:<13} {name}')
     print('')
     
+    # get container name
     name = input("Type container name, for which you'd like to set youtube id.\n")
     userContextId = None
     for identity in get_container_identities(profile):
@@ -122,19 +130,27 @@ def main():
         print('Incorrect name.')
         return
 
+    # get youtube id
     youtube_id = input("Type youtube id you'd like to set. (it's a 11 character text)\n")
     if not _correct_id(youtube_id):
         print('Incorrect id.\n')
         return 
     
-    try:
-        set_youtube_id(profile, f'^userContextId={userContextId}', youtube_id)
-    except sqlite3.OperationalError:
-        print("Couldn't set id, you must close firefox.")
-        return
+    # set youtube id to chosen container
+    set_youtube_id(profile, f'^userContextId={userContextId}', youtube_id)
     print('Id set successfully.')
     
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--profile', default='*.default',
+                        help='name of your firefox profile')
+    args = parser.parse_args()
+
+    try:
+        main(args.profile)
+    except OSError as err:
+        print(err)
+    except sqlite3.OperationalError:
+        print("Couldn't set id, you must close firefox.")
 
